f174b60757c3a8074cf399035748eeb0913a9e0d
Patrick Hein <bagbag98@googlemail.com>
2026-05-15 16:55:29 +0200

    enable nix linux-builder for cross-arch builds
    needed to deploy the aarch64 + x86_64 NixOS servers from this Mac.
    4 parallel jobs, ephemeral VM disk.

--- modules/darwin/nix.nix
+++ modules/darwin/nix.nix
@@ -23,4 +23,16 @@
       Minute = 0;
     };
   };
+
+  # Tiny aarch64-linux VM used as a remote builder so this Mac can build
+  # Linux derivations (e.g. for deploying NixOS hosts from another repo).
+  nix.linux-builder = {
+    enable = true;
+    ephemeral = true;
+    maxJobs = 4;
+    systems = [
+      "aarch64-linux"
+      "x86_64-linux"
+    ];
+  };
 }
